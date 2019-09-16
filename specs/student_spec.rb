87e0f2e280bf950f56e6d5d2c5219ac05b07128e
user <user@users-MacBook-Pro.local>
2019-09-16 14:56:33 +0100

    adds set_student_cohort

--- specs/student_spec.rb
+++ specs/student_spec.rb
@@ -11,7 +11,21 @@ class TestStudent < MiniTest::Test
 
 def test_get_student_cohort
   cohort = Student.new("Fish", "F23")
-  assert_equal("F23", cohort.get_cohort_name)
+  assert_equal("F23", cohort.get_student_cohort)
 end
+
+def test_set_student_name
+  student = Student.new("Fish", "F23")
+  student.set_student_name("Dr. Fish")
+  assert_equal("Dr. Fish", student.get_student_name)
+end
+
+def test_set_student_cohort
+  student = Student.new("Fish", "F23")
+  student.set_student_cohort("X99")
+  assert_equal("X99", student.get_student_cohort)
+end
+
+
 
 end
--- student.rb
+++ student.rb
@@ -4,10 +4,20 @@ def initialize(name, cohort)
   @cohort = cohort
 end
 
-  def get_student_name
+  def get_student_name()
     return @name
   end
 
+def get_student_cohort()
+  return @cohort
+end
 
+def set_student_name(new_name)
+  @name = new_name
+end
+
+def set_student_cohort(new_cohort)
+  @cohort = new_cohort
+end
 
 end
